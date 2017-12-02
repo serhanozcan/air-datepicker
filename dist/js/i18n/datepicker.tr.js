62cba94689bc83c40f0e230ec0c09cd8a07e9b18
Serhan Özcan <34198227+serhanozcan@users.noreply.github.com>
2017-12-03 02:44:41 +0300

    datepicker.tr.js
    Air Datepicker Turkish Lang.

---
+++ dist/js/i18n/datepicker.tr.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['tr'] = {
+    days: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+    daysShort: ["Pzr", "Pzt", "Sal", "Çrş", "Prş", "Cum", "Cmt"],
+    daysMin: ["Pa", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+    months: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+    monthsShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz", "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+    today: 'Bugün',
+    clear: 'Temizle',
+    dateFormat: 'dd/mm/yyyy',
+    timeFormat: 'hh:ii',
+    firstDay: 1
+}; })(jQuery);
